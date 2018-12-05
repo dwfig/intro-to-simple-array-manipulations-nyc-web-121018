23e29ed946f0d07b96961bde40714bbcf41a281e
Daniel Fries <daniel.w.fries@gmail.com>
2018-12-05 05:19:15 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array, string)
+##
+end
+
+def using_unshift
+##
+end
+
+def using_pop
+##
+end
+
+def pop_with_args(array)
+##
+end
+
+def using_unshift
+##
+end
+
+def shift_with_args(array)
+##
+end
+
+def using_concat(arr1, arr2)
+##
+end
+
+def using_insert(arr1, arr2)
+##
+end
+
+def using_uniq(array)
+##
+end
+
+def using_flatten(array)
+##
+end
+
+def using_delete(array, string)
+##
+end
+
+def using_delete_at(array, integer)
+##
+end  
